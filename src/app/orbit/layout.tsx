b9a05985a9165c1b24338ed9b21b76d4923640f0
Votre Nom <itachicryptoo@gmail.com>
2026-02-22 23:16:33 +0100

    revert: remove orbit from appforge

--- src/app/orbit/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function OrbitLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body style={{ margin: 0, padding: 0, background: '#0a0a1a' }}>
-        {children}
-      </body>
-    </html>
-  );
-}
